4209534b193e0d2ec03ecf4b4a70d04df3adda1c
Raúl Carayol Rodríguez <raul.carayol@insbaixcamp.cat>
2019-10-25 19:23:52 +0200

    Commit creacion clase Fibonacci

--- M9_UF2_Actividad2/src/activitat2.java
+++ M9_UF2_Actividad2/src/activitat2.java
@@ -7,37 +7,40 @@ import java.util.concurrent.TimeUnit;
 
 public class activitat2 {
 
-    public static void main(final String... args) throws InterruptedException, ExecutionException {
-        //mostrem hora actual abans d’execució
-    Calendar calendario = new GregorianCalendar();
-    System.out.println("Inici: "+ calendario.get(Calendar.HOUR_OF_DAY) + ":" + calendario.get(Calendar.MINUTE) +
-        ":" + calendario.get(Calendar.SECOND));
-    // Crea un pool de 4 fils
-    final ScheduledExecutorService schExService = Executors.newScheduledThreadPool(4);
-    // Crea objecte Runnable.
-    final Runnable ob = new activitat2().new ExecutaFil();
-    // Programa Fil, s’inicia als 5 segons i després es va executant cada 6 segons
-    schExService.scheduleWithFixedDelay(ob, 5, 6, TimeUnit.SECONDS);
-    // Espera per acabar 34 segons
-    schExService.awaitTermination(34, TimeUnit.SECONDS);
-    // shutdown (Acaba tots els fils)
-    schExService.shutdownNow();
-    //Mostra "Completat" al final del programa
-    System.out.println("Completat");
-    }
+	public static void main(final String... args) throws InterruptedException, ExecutionException {
+		//mostrem hora actual abans d’execució
+		Calendar calendario = new GregorianCalendar();
+		System.out.println("Inici: "+ calendario.get(Calendar.HOUR_OF_DAY) +
+				":" + calendario.get(Calendar.MINUTE) +
+				":" + calendario.get(Calendar.SECOND));
+		// Crea un pool de 4 fils
+		final ScheduledExecutorService schExService = Executors.newScheduledThreadPool(4);
+		// Crea objecte Runnable.
+		final Runnable ob = new activitat2().new ExecutaFil();
+		// Programa Fil, s’inicia als 5 segons i després es va executant cada 6 segons
+		schExService.scheduleWithFixedDelay(ob, 5, 6, TimeUnit.SECONDS);
+		// Espera per acabar 34 segons
+		schExService.awaitTermination(34, TimeUnit.SECONDS);
+		// shutdown (Acaba tots els fils)
+		schExService.shutdownNow();
+		//Mostra "Completat" al final del programa
+		System.out.println("Completat");
+	}
 
-    // Fil Runnable 
-    class ExecutaFil implements Runnable {
-        @Override
-        public void run() {
-        	
-            Calendar calendario = new GregorianCalendar();
-            System.out.println("Hora execució tasca: "+
-                calendario.get(Calendar.HOUR_OF_DAY) + ":" +
-                calendario.get(Calendar.MINUTE) + ":" +
-                calendario.get(Calendar.SECOND));
-            System.out.println("Tasca en execució");
-            System.out.println("Execució acabada");
-            }
-        }
-    }
+	// Fil Runnable 
+	class ExecutaFil implements Runnable {
+		@Override
+		public void run() {
+			
+			//Mostar el hilo en ejecución
+			System.out.println("  " + Thread.currentThread().getName());
+			Calendar calendario = new GregorianCalendar();
+			System.out.println("Hora execució tasca: "+
+					calendario.get(Calendar.HOUR_OF_DAY) + ":" +
+					calendario.get(Calendar.MINUTE) + ":" +
+					calendario.get(Calendar.SECOND));
+			System.out.println("Tasca en execució");
+			System.out.println("Execució acabada");
+		}
+	}
+}
---
+++ M9_UF2_Actividad3/src/Fibonacci.java
@@ -0,0 +1,17 @@
+
+public class Fibonacci {
+
+	public static long calculaFibonacci(long numero) {
+        double calcul = java.lang.Math.cos(54879854);
+        if (numero==0) { return 0; }
+        else if (numero==1) { return 1; }
+        else { 
+            return (calculaFibonacci(numero-2) + calculaFibonacci(numero-1)); 
+        }
+    }
+	
+	public static void main(String[] args) {
+
+	}
+
+}
