a0f6eba56634356a5b6060f2e35b6059af0c797c
Raúl Carayol Rodríguez <raul.carayol@insbaixcamp.cat>
2019-10-25 17:29:08 +0200

    commit 5

--- M9_UF2_Actividad2/src/activitat2.java
+++ M9_UF2_Actividad2/src/activitat2.java
@@ -18,15 +18,14 @@ public class activitat2 {
     final Runnable ob = new activitat2().new ExecutaFil();
     // Programa Fil, s’inicia als 5 segons i després es va executant cada 6 segons
     schExService.scheduleWithFixedDelay(ob, 5, 6, TimeUnit.SECONDS);
-    // Espera per acabar 10 segons
-    schExService.awaitTermination(10, TimeUnit.SECONDS);
+    // Espera per acabar 34 segons
+    schExService.awaitTermination(34, TimeUnit.SECONDS);
     // shutdown .
     schExService.shutdownNow();
     System.out.println("Completat");
     }
 
     // Fil Runnable 
-//mola
     class ExecutaFil implements Runnable {
     	
         @Override
