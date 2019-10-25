891a0c21145157227ed53c237b8d59afbd0bff76
Raúl Carayol Rodríguez <raul.carayol@insbaixcamp.cat>
2019-10-25 19:39:32 +0200

    commit fibonacci fork join Compute

--- M9_UF2_Actividad3/src/A_fibonacci_forkJoin.java
+++ M9_UF2_Actividad3/src/A_fibonacci_forkJoin.java
@@ -1,9 +1,23 @@
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
-public class A_fibonacci_forkJoin {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class A_fibonacci_forkJoin extends RecursiveTask<Long> {
+    long numero;
+    public A_fibonacci_forkJoin(long numero){
+        this.numero=numero;
+    }    
+    protected Long compute() {
+        // ATENCIO **1** double calcul = java.lang.Math.cos(54879854);
+        if(numero <= 1) return numero;
+        A_fibonacci_forkJoin fib1 = new A_fibonacci_forkJoin(numero-1);
+        //fib1.fork();
+	A_fibonacci_forkJoin fib2 = new A_fibonacci_forkJoin(numero-2);
+        fib2.fork();
+	 return fib1.compute()+ fib2.join();
+	 }
+    public static void main(String[] args){
+        ForkJoinPool pool = new ForkJoinPool();
+        System.out.println("Calculat:  " + pool.invoke(new A_fibonacci_forkJoin(35)));    
+    }
 }
