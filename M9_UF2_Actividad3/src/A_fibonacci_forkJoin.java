a85e4dd0e0501e0d8fb2f29f99070c15c488f422
RaulCarayol <raul.carayol@insbaixcamp.cat>
2019-10-31 16:19:51 +0100

    commit dia 31/10

--- M9_UF2_Actividad3/src/A_fibonacci_forkJoin.java
+++ M9_UF2_Actividad3/src/A_fibonacci_forkJoin.java
@@ -8,7 +8,7 @@ public class A_fibonacci_forkJoin extends RecursiveTask<Long> {
         this.numero=numero;
     }    
     protected Long compute() {
-        // ATENCIO **1** double calcul = java.lang.Math.cos(54879854);
+         double calcul = java.lang.Math.cos(54879854);
         if(numero <= 1) return numero;
         A_fibonacci_forkJoin fib1 = new A_fibonacci_forkJoin(numero-1);
         //fib1.fork();
@@ -17,11 +17,11 @@ public class A_fibonacci_forkJoin extends RecursiveTask<Long> {
 	 return fib1.compute()+ fib2.join();
 	 }
     public static void main(String[] args){
-    	long TiempoInicial=System.currentTimeMillis();
+    	long TiempoInicial=System.nanoTime();
         ForkJoinPool pool = new ForkJoinPool();
-        System.out.println("Calculat:  " + pool.invoke(new A_fibonacci_forkJoin(41)));
-		long tiempoEjecucion=((System.currentTimeMillis()-TiempoInicial));
-		System.out.println("Tiempo de ejecución : " +
-							tiempoEjecucion/100d);
+        System.out.println("Calculat:  " + pool.invoke(new A_fibonacci_forkJoin(32)));
+		long tiempoEjecucion=((System.nanoTime()-TiempoInicial));
+		System.out.println("Tiempo de ejecucion : " +
+							tiempoEjecucion);
     }
 }
--- M9_UF2_Actividad3/src/Fibonacci.java
+++ M9_UF2_Actividad3/src/Fibonacci.java
@@ -11,11 +11,11 @@ public class Fibonacci {
     }
 	
 	public static void main(String[] args) {
-		long TiempoInicial=System.currentTimeMillis();
-		System.out.println("Calculat : "+ calculaFibonacci(30));
-		long tiempoEjecucion=((System.currentTimeMillis()-TiempoInicial));
-		System.out.println("Tiempo de ejecución :" +
-							tiempoEjecucion/100d);
+		long TiempoInicial=System.nanoTime();
+		System.out.println("Calculat : "+ calculaFibonacci(42));
+		long tiempoEjecucion=((System.nanoTime()-TiempoInicial));
+		System.out.println("Tiempo de ejecucion :" +
+							tiempoEjecucion);
 	}
 
 }
