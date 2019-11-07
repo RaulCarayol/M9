f68f06dbf3e1a2df836627a1d59c50d3f5ef057d
Alumne <Alumne@104-PC03>
2019-11-07 17:19:43 +0100

    commit inicio dia 07/11/2019

--- M9_UF2_Actividad4/src/Apartat1.java
+++ M9_UF2_Actividad4/src/Apartat1.java
@@ -13,37 +13,43 @@ public class Apartat1 {
 		private String nombre;
 		private int articulos;
 		
+		public Cliente(String nombre, int articulos){
+			this.nombre= nombre;
+			this.articulos = articulos;
+		}
 		
 		@Override
 		public Cliente call() {
 
-			return new Cliente();
+			return new Cliente(nombre, articulos);
 		}
-		
-	}
-	
-	public static void main(String[] args) throws InterruptedException {
-		
-		//final ScheduledExecutorService schExService = Executors.newScheduledThreadPool(4);
-		
-		// Crea objecte Runnable.
-//		final Runnable ob = new Apartat1().new Cliente();
-//		schExService.scheduleWithFixedDelay(ob, 0, 3, TimeUnit.SECONDS);
-//		schExService.awaitTermination(150, TimeUnit.SECONDS);
-		
-		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
-		// Creamos una lista de sumas de dos numeros aleatorios
-		List<Cliente> llistaTasques= new ArrayList<Cliente>();
-		
-		for (int i = 0; i < 25; i++) {
-			Cliente cliente = new Cliente();
-			llistaTasques.add(Cliente);
-		}
-		
-	
-		//schExService.shutdownNow();
-		System.out.println("Completat");
 		
 	}
 
+
+
+public static void main(String[] args) throws InterruptedException {
+	
+	//final ScheduledExecutorService schExService = Executors.newScheduledThreadPool(4);
+	
+	// Crea objecte Runnable.
+//	final Runnable ob = new Apartat1().new Cliente();
+//	schExService.scheduleWithFixedDelay(ob, 0, 3, TimeUnit.SECONDS);
+//	schExService.awaitTermination(150, TimeUnit.SECONDS);
+	
+	ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
+	// Creamos una lista de sumas de dos numeros aleatorios
+	ArrayList<Cliente> llistaTasques= new ArrayList<Cliente>();
+	
+	for (int i = 0; i < 25; i++) {
+		//Cliente cliente = new Cliente("mi", 3);
+		//llistaTasques.add(cliente);
+	}
+	
+
+	//schExService.shutdownNow();
+	System.out.println("Completat");
+	
+
+}
 }
---
+++ M9_UF2_Actividad5/src/HeretaFil.java
@@ -0,0 +1,46 @@
+public class HeretaFil extends Thread {
+
+	String strImprimir;
+	public HeretaFil(String strP) {
+		strImprimir=strP;
+	}
+
+	public void run(){
+		for(int x=0;x<5;x++){
+			System.out.println(strImprimir+ " " + x);
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		Thread primer = new HeretaFil("Fil 1");
+		Thread segon = new HeretaFil("Fil 2");
+		Thread tercero = new HeretaFil("Fil 3");
+		Thread cuarto = new HeretaFil("Fil 4");
+		Thread quinto = new HeretaFil("Fil 5");
+		Thread sexto = new HeretaFil("Fil 6");
+		Thread septimo = new HeretaFil("Fil 7");
+		Thread octavo = new HeretaFil("Fil 8");
+		Thread noveno = new HeretaFil("Fil 9");
+		Thread decimo = new HeretaFil("Fil 10");
+		Thread undecimo = new HeretaFil("Fil 11");
+		
+		// Hem creat dos fils primer i segon, però no s’han executat.
+		// Per poder−lo executar s’ha de cridar al mètode start()
+		primer.start();
+		segon.start();
+		tercero.start();
+		cuarto.start();
+		quinto.start();
+		sexto.start();
+		septimo.start();
+		octavo.start();
+		noveno.start();
+		decimo.start();
+		undecimo.start();
+
+		System.out.println("Final Fil Principal");
+
+	}
+}
