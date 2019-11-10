042f6a14a5d16a45a9ac6ca8c6f4c3bcc808d114
avata <avata@DESKTOP-UKBJND9>
2019-11-10 21:40:54 +0100

    commit

--- M9_UF2_Actividad4/src/Apartat1.java
+++ M9_UF2_Actividad4/src/Apartat1.java
@@ -8,13 +8,21 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+class Cliente{
+	String nombre;
+	int[] articulos;
 
+	public Cliente(String nombre, int[] articulos){
+		this.nombre= nombre;
+		this.articulos = articulos;
+	}
+}
 public class Apartat1 implements Runnable{
 	Cliente cliente;
 	public Apartat1(Cliente cliente){
 		this.cliente=cliente;
 	}
-	
+
 	@Override
 	public void run() {
 		System.out.print("Generando el " + cliente.nombre + " con " + cliente.articulos.length + " artículos ");
@@ -30,60 +38,42 @@ public class Apartat1 implements Runnable{
 		System.out.println();
 		System.out.println(cliente.nombre + " pasando por caja...");
 		for (int i = 0; i < cliente.articulos.length; i++) {
-				try {
-					Thread.sleep(cliente.articulos[i] * 1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-	}
-}
- class Cliente{
-	 String nombre;
-	 int[] articulos;
-	
-	public Cliente(String nombre, int[] articulos){
-		this.nombre= nombre;
-		this.articulos = articulos;
-	}
-	
-
-	
- }
-public void main(String[] args) throws InterruptedException {
-	
-	//final ScheduledExecutorService schExService = Executors.newScheduledThreadPool(4);
-	
-	// Crea objecte Runnable.
-//	final Runnable ob = new Apartat1().new Cliente();
-//	schExService.scheduleWithFixedDelay(ob, 0, 3, TimeUnit.SECONDS);
-//	schExService.awaitTermination(150, TimeUnit.SECONDS);
-	
-	
-	ExecutorService executor1 = Executors.newFixedThreadPool(10);
-	ArrayList<Cliente> llistaTasques= new ArrayList<Cliente>();
-	ArrayList<Cliente> clientes = new ArrayList<Cliente>();
-	
-	for (int i = 0; i < 50; i++) {  
-		int[] Articulos = new int[(int) (Math.random() * 30 - 1) + 1];
-		for (int j = 0; j < Articulos.length; j++) {
-			Articulos[j] = (int) (Math.random() * 8 - 1) + 1;
+			try {
+				Thread.sleep(cliente.articulos[i] * 1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-
-		clientes.add(new Cliente("Cliente num " + (i + 1) , Articulos));
-	}  
-
-	for (Cliente cliente: clientes) {
-		Thread.sleep(3000);
-		Runnable caja = new Apartat1(cliente);
-		executor1.execute(caja);
 	}
-	executor1.shutdown();
 
-	//schExService.shutdownNow();
-	System.out.println("Completat");
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ExecutorService executor1 = Executors.newFixedThreadPool(10);
+		ArrayList<Cliente> llistaTasques= new ArrayList<Cliente>();
+		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+
+		for (int i = 0; i < 50; i++) {  
+			int[] Articulos = new int[(int) (Math.random() * 30 - 1) + 1];
+			for (int j = 0; j < Articulos.length; j++) {
+				Articulos[j] = (int) (Math.random() * 8 - 1) + 1;
+			}
+
+			clientes.add(new Cliente("Cliente num " + (i + 1) , Articulos));
+		}  
+		for (Cliente cliente: clientes) {
+			Thread.sleep(3000);
+			Runnable caja = new Apartat1(cliente);
+			executor1.execute(caja);
+		}
+		executor1.shutdown();
+
+		//schExService.shutdownNow();
+		System.out.println("Completat");
 	}
 }
+
 
 
 
