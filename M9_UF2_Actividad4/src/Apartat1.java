7f3a0b9e36f48a1080c97c536f3daffc57deb45a
avata <avata@DESKTOP-UKBJND9>
2019-11-10 21:19:27 +0100

    commit

--- M9_UF2_Actividad4/src/Apartat1.java
+++ M9_UF2_Actividad4/src/Apartat1.java
@@ -14,29 +14,29 @@ public class Apartat1 implements Runnable{
 	public Apartat1(Cliente cliente){
 		this.cliente=cliente;
 	}
-	public class Cliente implements Callable<Cliente>{
-		private String nombre;
-		private int[] articulos;
-		
-		public Cliente(String nombre, int[] articulos){
-			this.nombre= nombre;
-			this.articulos = articulos;
-		}
-		
-		@Override
-		public Cliente call() {
-
-			return new Cliente(nombre, articulos);
-		}
-		
-	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		
 	}
+}
+ class Cliente implements Callable<Cliente>{
+	private String nombre;
+	private int[] articulos;
+	
+	public Cliente(String nombre, int[] articulos){
+		this.nombre= nombre;
+		this.articulos = articulos;
+	}
+	
+	@Override
+	public Cliente call() {
 
+		return new Cliente(nombre, articulos);
+	}
+	
+ 
 public static void main(String[] args) throws InterruptedException {
 	
 	//final ScheduledExecutorService schExService = Executors.newScheduledThreadPool(4);
@@ -49,13 +49,28 @@ public static void main(String[] args) throws InterruptedException {
 	
 	ExecutorService executor1 = Executors.newFixedThreadPool(10);
 	ArrayList<Cliente> llistaTasques= new ArrayList<Cliente>();
+	ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+	
+	for (int i = 0; i < 50; i++) {  
+		int[] Articulos = new int[(int) (Math.random() * 30 - 1) + 1];
+		for (int j = 0; j < Articulos.length; j++) {
+			Articulos[j] = (int) (Math.random() * 8 - 1) + 1;
+		}
 
+		clientes.add(new Cliente("Cliente num " + (i + 1) , Articulos));
+	}  
+
+	for (Cliente cliente: clientes) {
+		Thread.sleep(3000);
+		Runnable caja = new Apartat1(cliente);
+		executor1.execute(caja);
+	}
 
 	//schExService.shutdownNow();
 	System.out.println("Completat");
-	
-
+	}
 }
 
 
-}
+
+
