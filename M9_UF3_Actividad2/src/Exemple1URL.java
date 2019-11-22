907c119c38fe1e8ae869d167315b901204993f99
Alumne <Alumne@104-PC03>
2019-11-22 17:25:41 +0100

    commit

--- M9_UF3_Actividad2/src/Exemple1URL.java
+++ M9_UF3_Actividad2/src/Exemple1URL.java
@@ -1,39 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
 
+import org.omg.CORBA.portable.InputStream;
+
 public class Exemple1URL {
-	
-	public static void main (String[] args) {
-		URL url;
-		
+
+	public static void main (String[] args) throws MalformedURLException {
+		URL url = null;
+		if(args.length==0){
+			url = new URL("http://www.insbaixcamp.cat/moodle/");
+		}else if(args.length==1){
+			url= new URL(args[0]);
+		}else if(args.length==2){
+			String strurl =args[0];
+			int port = Integer.valueOf(args[1]);
+			
+			
+		}else{
+			url = new URL(args[0],args[1],Integer.valueOf(args[2]),args[3]);
+		}
+
+//			System.out.println("Constructor per a protocol + URL + port + directori: ");
+//			url = new URL("http", "doc.oracle.com", 80, "/javase/7");
+			Visualitzar (url);
+//			System.out.println("Constructor per a un objecte URL i un directori: ");
+//			URL urlBase = new URL("http://docs.oracle.com/");
+//			url = new URL(urlBase, "/javase/7/docs/api/java/net/URL.html");
+//			Visualitzar (url);
+
+		BufferedReader in;
+
 		try {
-			
-			System.out.println("Constructor simple per a un URL: ");
-			url = new URL("http://docs.oracle.com/");
-			Visualitzar (url);
-			
-			System.out.println("Altra constructor simple per a un URL: ");
-			url = new URL("http://localhost/moodle/");
-			Visualitzar (url);
-			
-			System.out.println("Constructor per a protocol + URL + directori: ");
-			url = new URL("http", "doc.oracle.com", "/javase/7");
-			Visualitzar (url);
-			
-			System.out.println("Constructor per a protocol + URL + port + directori: ");
-			url = new URL("http", "doc.oracle.com", 80, "/javase/7");
-			Visualitzar (url);
-			
-			System.out.println("Constructor per a un objecte URL i un directori: ");
-			URL urlBase = new URL("http://docs.oracle.com/");
-			url = new URL(urlBase, "/javase/7/docs/api/java/net/URL.html");
-			Visualitzar (url);
-			
-		} catch (MalformedURLException e) { System.out.println(e); }
-		
+			java.io.InputStream inputStream = url.openStream();
+			in = new BufferedReader(new InputStreamReader(inputStream));
+
+			String inputLine;
+
+			while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+			in.close();
+
+		} catch (IOException e) {e.printStackTrace(); }
+
 	}
-	
+
 	private static void Visualitzar(URL url) {
-		
+
 		System.out.println("\tURL complerta: "+url.toString());
 		System.out.println("\tgetProtocol: "+url.getProtocol());
 		System.out.println("\tgetHost: "+url.getHost());
