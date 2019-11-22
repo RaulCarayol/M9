3c760e566878cbcf32af100abd3fa89349f97570
Alumne <Alumne@104-PC03>
2019-11-22 17:49:44 +0100

    commit

--- M9_UF3_Actividad2/src/Exemple1URL.java
+++ M9_UF3_Actividad2/src/Exemple1URL.java
@@ -1,9 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.*;
-
-import org.omg.CORBA.portable.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Exemple1URL {
 
@@ -16,33 +16,13 @@ public class Exemple1URL {
 		}else if(args.length==2){
 			String strurl =args[0];
 			int port = Integer.valueOf(args[1]);
-			
-			
-		}else{
-			url = new URL(args[0],args[1],Integer.valueOf(args[2]),args[3]);
+
+			url = new URL(strurl);
+			url = new URL(url.getProtocol(),url.getHost(),port,url.getFile());
 		}
 
-//			System.out.println("Constructor per a protocol + URL + port + directori: ");
-//			url = new URL("http", "doc.oracle.com", 80, "/javase/7");
-			Visualitzar (url);
-//			System.out.println("Constructor per a un objecte URL i un directori: ");
-//			URL urlBase = new URL("http://docs.oracle.com/");
-//			url = new URL(urlBase, "/javase/7/docs/api/java/net/URL.html");
-//			Visualitzar (url);
-
-		BufferedReader in;
-
-		try {
-			java.io.InputStream inputStream = url.openStream();
-			in = new BufferedReader(new InputStreamReader(inputStream));
-
-			String inputLine;
-
-			while ((inputLine = in.readLine()) != null)
-				System.out.println(inputLine);
-			in.close();
-
-		} catch (IOException e) {e.printStackTrace(); }
+		Visualitzar (url);
+		Codigo(url);
 
 	}
 
@@ -58,6 +38,23 @@ public class Exemple1URL {
 		System.out.println("\tgetAuthority: "+url.getAuthority());
 		System.out.println("\tgetQuery: "+url.getQuery());
 		System.out.println("=====================================================");
+	}
+
+
+	private static void Codigo(URL url) {
+		BufferedReader in;
+
+		try {
+
+			InputStream inputStream = url.openStream();
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			String inputLine;
+
+			while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+			in.close();
+
+		} catch (IOException e) {e.printStackTrace(); }
 	}
 
 }
