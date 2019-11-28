a02ab829d980d8be604bf4ff366f570ea4134046
Alumne <Alumne@104-PC03>
2019-11-28 18:20:40 +0100

    commit timeout

--- M9_UF3_Actividad4/src/ClientUDP2.java
+++ M9_UF3_Actividad4/src/ClientUDP2.java
@@ -3,44 +3,56 @@ import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.TimeoutException;
+
+import org.omg.CORBA.TIMEOUT;
 public class ClientUDP2 {
 
-public static void main (String[] args) throws Exception {
+	public static void main (String[] args) throws Exception {
+		boolean seguir = true;
+
+		//FLUX PER A ENTRADA ESTÀNDARD
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		//Socket client
+		DatagramSocket clientSocket = new DatagramSocket();
+		byte[] enviats = new byte[1024];
+		byte[] rebuts = new byte[1024];
 		
-	//FLUX PER A ENTRADA ESTÀNDARD
-			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-			//Socket client
-			DatagramSocket clientSocket = new DatagramSocket();
-			byte[] enviats = new byte[1024];
-			byte[] rebuts = new byte[1024];
-			
-			//DADES DEL SERVIDOR al qual s'envia el missatge
-			InetAddress IPServidor = InetAddress.getLocalHost();
-			int port = 9800;
-			
-			//INTRODUIR DADES PEL TECLAT
-			System.out.print("Introdueix missatge: ");
-			String cadena = in.readLine();
-			enviats = cadena.getBytes();
-			
-			//ENVIANT DATAGRAMA AL SERVIDOR
-			System.out.println("Enviant "+enviats.length+"bytes al servidor.");
-			DatagramPacket enviament = new DatagramPacket(enviats, enviats.length, IPServidor, port);
-			clientSocket.send(enviament);
-			
-			//REBENT DATAGRAMA DEL SERVIDOR
-			DatagramPacket rebut = new DatagramPacket(rebuts, rebuts.length);
-			System.out.println("Esperant datagrama...");
+		//DADES DEL SERVIDOR al qual s'envia el missatge
+		InetAddress IPServidor = InetAddress.getLocalHost();
+		int port = 9800;
+		while(seguir){
+		//INTRODUIR DADES PEL TECLAT
+		System.out.print("Introdueix missatge: ");
+		String cadena = in.readLine();
+		enviats = cadena.getBytes();
+
+		//ENVIANT DATAGRAMA AL SERVIDOR
+		System.out.println("Enviant "+enviats.length+"bytes al servidor.");
+		DatagramPacket enviament = new DatagramPacket(enviats, enviats.length, IPServidor, port);
+		clientSocket.send(enviament);
+
+		//REBENT DATAGRAMA DEL SERVIDOR
+		DatagramPacket rebut = new DatagramPacket(rebuts, rebuts.length);
+		System.out.println("Esperant datagrama...");
+		clientSocket.setSoTimeout(5000);
+		try{
 			clientSocket.receive(rebut);
 			String majuscula = new String(rebut.getData());
-			
+	
 			//ACONSEGUINT INFORMACIÓ DEL DATAGRAMA
 			InetAddress IPOrigen = rebut.getAddress();
 			int portOrigen = rebut.getPort();
 			System.out.println("\tProcedent de: "+IPOrigen+":"+portOrigen);
 			System.out.println("\tDades: "+majuscula.trim());
-			//Tanca el socket
-			clientSocket.close();
-			
+		}catch(SocketTimeoutException e){
+			  	seguir=false;
+			  	System.out.println("Tiempo excedido");
+			 }
 		}
+		//Tanca el socket
+		clientSocket.close();
+
+	}
 }
