14b21366ee6eaf099429407d0230f5ff2751cac1
Alumne <Alumne@104-PC03>
2019-11-28 17:43:16 +0100

    commit clase servidor

--- M9_UF3_Actividad4/src/ClientUDP2.java
+++ M9_UF3_Actividad4/src/ClientUDP2.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-public class M9_UF3_Actividad4 {
+public class ClientUDP2 {
 
 public static void main (String[] args) throws Exception {
 		
@@ -43,7 +43,4 @@ public static void main (String[] args) throws Exception {
 			clientSocket.close();
 			
 		}
-		
-
 }
-
